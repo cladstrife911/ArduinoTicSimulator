b231e13218620149496bae0911e6ecfa013681dc
antoine m <cladstrife911@yahoo.fr>
2020-07-10 16:57:26 +0200

    Update to match real Linky behavior

--- src/ArduinoTicSimulator/ArduinoTicSimulator.ino
+++ src/ArduinoTicSimulator/ArduinoTicSimulator.ino
@@ -4,12 +4,14 @@
 #define UART_BAUDRATE (1200)
 #define UART_CONFIG (SERIAL_7E1)
 /*delay between TIC information in ms*/
-#define DELAY_BETWEEN_TIC (1000)
+#define DELAY_BETWEEN_TIC (10)
 //Serial1 TX pin is TX1
 #define UART_SERIAL Serial1
 
 #define ASCII_ETX (0x03)
 #define ASCII_STX (0x02)
+
+#define MAX_PAPP_VAL 6000
 
 /******* local typedef *****/
 typedef struct
@@ -47,14 +49,26 @@ void loop() {
 /******* local functions definition *****/
 void vidPrepareTicInfo(void)
 {
+  char tmpBuff[5];
+  
   LOC_strTicInfo.HCHC = LOC_strTicInfo.HCHC+random(10);
   LOC_strTicInfo.HCHP = LOC_strTicInfo.HCHP+random(10);
+  LOC_strTicInfo.PAPP = random(MAX_PAPP_VAL);
   LOC_strTicInfo.IINST = random(10,20);
+  LOC_strTicInfo.PTEC = random(2); /*0 or 1*/
+
+  if(LOC_strTicInfo.PTEC){
+    sprintf(tmpBuff,"HP.."); 
+  }else{
+    sprintf(tmpBuff,"HC..");
+  }
   sprintf(acUartTxBuff, \
-           "ADCO 031961353121 <\r\nOPTARIF HC.. <\r\nISOUSC 60 <\r\nHCHC %09d #\r\nHCHP %09d :\r\nPTEC HP..  \r\nIINST %03d X\r\nIMAX 090 H\r\nPAPP 02260 +\r\nHHPHC A ,\r\nMOTDETAT 000000 B%c%c", \
+           "ADCO 031961353121 <\r\nOPTARIF HC.. <\r\nISOUSC 60 <\r\nHCHC %09d #\r\nHCHP %09d :\r\nPTEC %s  \r\nIINST %03d X\r\nIMAX 090 H\r\nPAPP %05d +\r\nHHPHC A ,\r\nMOTDETAT 000000 B%c%c", \
            LOC_strTicInfo.HCHC, \
            LOC_strTicInfo.HCHP, \
+           tmpBuff,\
            LOC_strTicInfo.IINST, \
+           LOC_strTicInfo.PAPP, \
            ASCII_ETX, ASCII_STX); 
    
 }
